d563d54418e8fc2e87732086ae27817eb7826db6
Yang <noriyang0215@gmail.com>
2023-03-18 18:07:54 +0800

    add filters

--- src/main.js
+++ src/main.js
@@ -4,14 +4,17 @@ import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 
-// Import component
 import Loading from 'vue3-loading-overlay'
-// Import stylesheet
 import 'vue3-loading-overlay/dist/vue3-loading-overlay.css'
 
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
+import { currency } from '@/methods/filters'
+
 const app = createApp(App)
+app.config.globalProperties.$filters = {
+  currency
+}
 app.component('Loading', Loading)
 app.use(router)
 app.use(VueAxios, axios)
